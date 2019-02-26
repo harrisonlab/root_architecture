ed3ddf6623a9f3d01ebe134538e7ac31c2f1af55
MagdaCM <Magdalena.Cobo-Medina@emr.ac.uk>
2019-02-26 17:50:37 +0000

    beagle

--- Fine_mapping/scripts/sub_beagle.sh
+++ Fine_mapping/scripts/sub_beagle.sh
@@ -1,15 +1,17 @@
+
 #$ -S /bin/bash
 #$ -cwd
 #$ -pe smp 4
 #$ -l h_vmem=3G
-#$ -l h=blacklace01.blacklace|blacklace02.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace|blacklace12.blacklace
+#$ -l h=blacklace01.blacklace|blacklace03.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace|blacklace12.blacklace
 
-#Script will phase diploid genotypes in a given VCF file. 
+#Script will phase diploid genotypes in a given VCF file.
 #Output: VCF file with phased genotypes with the suffix "_haplo"
 
 input=$1
+pedigree=$2
 output=${input%.vcf}_haplo
-java=/home/sobczm/bin/jre1.8.0_101/bin/java
-beagle=/home/sobczm/bin/beagle/beagle.27Jul16.86a.jar
+#java=/home/sobczm/bin/jre1.8.0_101/bin/java
+beagle=/home/gomeza/prog/beagle/beagle.r1399.jar
 
-$java -Xmx8g -Xms1g -jar $beagle gt=$input out=$output nthreads=20 window=1000 overlap=100 gprobs=false
+java -jar $beagle gt=$input ped=$pedigree out=$output ibd=true
